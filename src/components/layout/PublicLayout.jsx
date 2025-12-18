048be7011a9947440449a18490ebb4953a594026
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-12-18 21:31:47 +0530

    fix the error from social media tags

---
+++ src/components/layout/PublicLayout.jsx
@@ -0,0 +1,24 @@
+import TopNav from './TopNav';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
+import PageLoader from '../common/PageLoader';
+import { usePageLoader } from '../../hooks/usePageLoader';
+import SocialMediaFloating from '../common/SocialMediaFloating';
+
+const PublicLayout = () => {
+  const { isLoading } = usePageLoader();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <PageLoader isVisible={isLoading} />
+      <SocialMediaFloating />
+      <TopNav />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default PublicLayout;
