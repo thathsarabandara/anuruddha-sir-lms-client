19224970327e1f300e2d29492f700f6bbd326cf7
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-12-18 21:00:47 +0530

    created authenticated layout

---
+++ src/components/layout/AuthenticatedLayout.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+import DashboardTopBar from './DashboardTopBar';
+import DashboardFooter from './DashboardFooter';
+import { Outlet } from 'react-router-dom';
+
+const AuthenticatedLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+        <main className="flex-1 overflow-y-auto bg-gray-50 flex flex-col">
+          <DashboardTopBar onMenuToggle={() => setIsSidebarOpen(!isSidebarOpen)} />
+          <div className="flex-1">
+            <Outlet />
+          </div>
+          <DashboardFooter />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticatedLayout;
