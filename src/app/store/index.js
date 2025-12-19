5827c1e0695fd5046a8386cc9759fdd0b37913bb
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-12-19 14:05:04 +0530

    created the redux store index file

---
+++ src/app/store/index.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/authSlice';
+import notificationReducer from '../slices/notificationSlice';
+import languageReducer from '../slices/languageSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    notification: notificationReducer,
+    language: languageReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default store;
