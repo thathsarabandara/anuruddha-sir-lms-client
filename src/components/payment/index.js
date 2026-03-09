01f99dc25a283ec2a380281903ddb5ef16e3ad8f
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-03-09 11:41:46 +0530

    add payment components export index for easy importing

---
+++ src/components/payment/index.js
@@ -0,0 +1,9 @@
+// Payment Components Export Index
+// All payment-related components bundled for easy importing
+
+export { default as CouponValidator } from './CouponValidator';
+export { default as EnrollmentKeyRedeemer } from './EnrollmentKeyRedeemer';
+export { default as AdminCouponManager } from './AdminCouponManager';
+export { default as AdminEnrollmentKeyManager } from './AdminEnrollmentKeyManager';
+export { default as TeacherCouponManager } from './TeacherCouponManager';
+export { default as StudentPurchaseHistory } from './StudentPurchaseHistory';
