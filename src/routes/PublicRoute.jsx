85f987a94cc2bb1d866cdc6ff9c5c6a8aa5919a0
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-03-12 12:56:49 +0530

    refactor: update PublicRoute to use ROLES constants for role-based navigation

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,17 +1,17 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { ROUTES } from '../utils/constants';
+import { ROUTES, ROLES } from '../utils/constants';
 
 const PublicRoute = ({ children }) => {
   const { isAuthenticated, user } = useSelector((state) => state.auth);
 
   if (isAuthenticated && user?.role) {
     const dashboardRoutes = {
-      STUDENT: ROUTES.STUDENT_DASHBOARD,
-      TEACHER: ROUTES.TEACHER_DASHBOARD,
-      ADMIN: ROUTES.ADMIN_DASHBOARD,
-      SUPER_ADMIN: ROUTES.ADMIN_DASHBOARD,
-      DEVELOPER: ROUTES.DEVELOPER_DASHBOARD,
+      [ROLES.STUDENT]: ROUTES.STUDENT_DASHBOARD,
+      [ROLES.TEACHER]: ROUTES.TEACHER_DASHBOARD,
+      [ROLES.ADMIN]: ROUTES.ADMIN_DASHBOARD,
+      [ROLES.SUPERADMIN]: ROUTES.ADMIN_DASHBOARD,
+      [ROLES.DEVELOPER]: ROUTES.DEVELOPER_DASHBOARD,
     };
     console.log('Redirecting to:', dashboardRoutes[user.role]);
     return <Navigate to={dashboardRoutes[user.role] || ROUTES.HOME} replace />;
