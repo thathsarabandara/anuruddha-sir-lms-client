c8783796cea3fd6ef48cf6ba112e7b4430af47dd
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-03-16 08:39:07 +0530

    fix: update import paths for TopNav and Footer components

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,5 +1,5 @@
-import TopNav from './TopNav';
-import Footer from './Footer';
+import TopNav from '../common/TopNav';
+import Footer from '../common/Footer';
 import { Outlet } from 'react-router-dom';
 import PageLoader from '../common/PageLoader';
 import { usePageLoader } from '../../hooks/usePageLoader';
