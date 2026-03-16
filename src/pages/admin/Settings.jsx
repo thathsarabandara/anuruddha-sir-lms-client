c0d9721900e23ce0d15526c1a12bb57386223840
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-03-16 14:49:33 +0530

    feat: remove unused PulseLoader import from AdminSettings component

--- src/pages/admin/Settings.jsx
+++ src/pages/admin/Settings.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FaFileVideo } from 'react-icons/fa';
-import PulseLoader from '../../components/common/PulseLoader';
+
 
 const AdminSettings = () => {
   const [activeTab, setActiveTab] = useState('general');
